d2ee40db812b5751b663444b48416e2bd9da95b9
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-05-26 18:56:51 +0900

    #23 설정 화면 구성

--- i_pet_pal/lib/screens/setting_screen.dart
+++ i_pet_pal/lib/screens/setting_screen.dart
@@ -1,13 +1,65 @@
+import 'package:settings_ui/settings_ui.dart';
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
+bool serverInference = false;
+
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
   // This widget is the root of your application.
   @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Hallo"),
+    return Scaffold(
+      body: SettingsList(
+        lightTheme: SettingsThemeData(
+          settingsListBackground: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        darkTheme: SettingsThemeData(
+          settingsListBackground: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        sections: [
+          SettingsSection(
+            title: const Text('인공지능'),
+            tiles: <SettingsTile>[
+              SettingsTile.switchTile(
+                title: const Text('서버에서 검사'),
+                initialValue: serverInference,
+                onToggle: (value) {
+                  setState(() {
+                    serverInference = !serverInference;
+                  });
+                },
+                leading: const Icon(Icons.leak_add_outlined),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: Text('계정'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: Icon(Icons.logout),
+                title: Text('로그아웃'),
+                onPressed: ((context) {}),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: Text('기타'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: Icon(Icons.star),
+                title: Text('앱 평가하기'),
+                onPressed: ((context) {}),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
