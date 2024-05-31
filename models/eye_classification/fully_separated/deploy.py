1d55eb25f36e8cc5fa55a2b18a0232072d401bc5
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-05-31 12:32:08 +0900

    #5 모델 파일 배포시 영어로 변환

--- models/eye_classification/fully_separated/deploy.py
+++ models/eye_classification/fully_separated/deploy.py
@@ -1,8 +1,9 @@
 import os
 import glob
 import shutil
-from pathlib import Path
 import tqdm
+from pathlib import Path
+from shared import desease_to_english
 
 if __name__ == "__main__":
     deploy_path = "deploy"
@@ -11,8 +12,10 @@ if __name__ == "__main__":
     for onnx_path in tqdm.tqdm(glob.glob(f"working\\*\\*.onnx")):
         model_name = Path(onnx_path).stem
         _, desease = os.path.split(os.path.dirname(onnx_path))
-        shutil.copyfile(onnx_path, f"{deploy_path}\\{desease}_{model_name}.onnx")
+        shutil.copyfile(
+            onnx_path, f"{deploy_path}\\{desease_to_english(desease)}_{model_name}.onnx"
+        )
 
     for label_file in tqdm.tqdm(glob.glob(f"working\\*\\label.txt")):
         _, desease = os.path.split(os.path.dirname(label_file))
-        shutil.copyfile(label_file, f"{deploy_path}\\{desease}.txt")
+        shutil.copyfile(label_file, f"{deploy_path}\\{desease_to_english(desease)}.txt")
