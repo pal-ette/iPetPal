65376b63332243e8209819dda4c3bb15a8642bf2
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-06-04 11:06:43 +0900

    #3 눈 서버 분류 적용

--- i_pet_pal/lib/api/triton_client.dart
+++ i_pet_pal/lib/api/triton_client.dart
@@ -9,6 +9,7 @@ import 'package:i_pet_pal/env/env.dart';
 
 class TritonClient {
   static const headers = {
+    "Content-Type": "application/json; charset=UTF-8",
     "pal-ette-pass": Env.palEtteApiKey,
   };
   static Future<bool> ping(String baseUrl) async {
@@ -26,25 +27,25 @@ class TritonClient {
       targetWidth: 224,
     );
     final image = (await codec.getNextFrame()).image;
-    final rgbFloats = await imageToFloatTensor(image);
+    final rgbFloats = await imageToFloatTensorTf(image);
     final imageTensor = OrtValueTensor.createTensorWithDataList(
-        Float32List.fromList(rgbFloats), [1, 3, 224, 224]);
+        Float32List.fromList(rgbFloats), [1, 224, 224, 3]);
 
     final url = Uri.parse("$baseUrl/models/$modelName/infer");
     final body = {
       "inputs": [
         {
-          "name": "data",
+          "name": "input_1",
           "datatype": "FP32",
-          "shape": [3, 224, 224],
+          "shape": [224, 224, 3],
           "data": imageTensor.value,
         },
       ],
       "outputs": [
         {
-          "name": "resnetv24_dense0_fwd",
+          "name": "dense_1",
           "parameters": {
-            "classification": 11,
+            "classification": 4,
           },
         }
       ]
@@ -58,16 +59,16 @@ class TritonClient {
       return List<(String, double)>.empty();
     }
 
-    final responseBody = jsonDecode(response.body);
+    final responseBody = json.decode(utf8.decode(response.bodyBytes));
     final result = List<(String, double)>.from(
         responseBody["outputs"][0]["data"].map((data) {
       final c = data.split(":");
-      return (c[2], double.parse(c[0]));
+      return (c[2].trim(), double.parse(c[0]));
     }));
     final values = result.map((e) => e.$2);
     final softmaxValues = softmax(values.toList());
     return List<(String, double)>.generate(
-      5,
+      4,
       (index) => (result[index].$1, softmaxValues[index]),
     );
   }
@@ -78,6 +79,19 @@ class TritonClient {
     return list.map((value) {
       return exp(value - C) / d;
     }).toList();
+  }
+
+  static Future<List<double>> imageToFloatTensorTf(ui.Image image) async {
+    final imageAsFloatBytes =
+        (await image.toByteData(format: ui.ImageByteFormat.rawRgba))!;
+    final rgbaUints = Uint8List.view(imageAsFloatBytes.buffer);
+    final indexed = rgbaUints.indexed;
+    return [
+      ...indexed.where((e) => [0, 1, 2].contains(e.$1 % 4)).map((e) {
+        var processed = e.$2.toDouble() / 225.0;
+        return processed;
+      })
+    ];
   }
 
   static Future<List<double>> imageToFloatTensor(ui.Image image) async {
