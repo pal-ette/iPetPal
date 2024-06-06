9caab3accaf06219cc77ee81416867b3445fe907
ryugibo <7928632+ryugibo@users.noreply.github.com>
2024-06-06 15:19:01 +0900

    #7 폰트 적용

--- i_pet_pal/lib/screens/info_screen.dart
+++ i_pet_pal/lib/screens/info_screen.dart
@@ -4,12 +4,6 @@ import 'package:url_launcher/url_launcher.dart';
 class InfoScreen extends StatelessWidget {
   const InfoScreen({super.key});
 
-  void _onTapNounProject() async {
-    await launchUrl(
-      Uri.parse("https://thenounproject.com/browse/icons/term/puppy/"),
-    );
-  }
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,30 +14,48 @@ class InfoScreen extends StatelessWidget {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("https://github.com/pal-ette/iPetPal"),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  const Text("puppy by IronSV from "),
-                  GestureDetector(
-                    onTap: _onTapNounProject,
-                    child: const Text(
-                      "Noun Project",
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ),
-                  const Text(" (CC BY 3.0)"),
-                ],
-              ),
-              //
+              for (final widget in [
+                const Text("https://github.com/pal-ette/iPetPal"),
+                _iconLicense(),
+                _fontLicense(),
+              ]) ...[
+                widget,
+                const SizedBox(
+                  height: 20,
+                ),
+              ]
             ],
           ),
         ],
       ),
     );
+  }
+
+  void _onTapNounProject() async {
+    await launchUrl(
+      Uri.parse("https://thenounproject.com/browse/icons/term/puppy/"),
+    );
+  }
+
+  Widget _iconLicense() {
+    return Row(
+      children: [
+        const Text("puppy by IronSV from "),
+        GestureDetector(
+          onTap: _onTapNounProject,
+          child: const Text(
+            "Noun Project",
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+        const Text(" (CC BY 3.0)"),
+      ],
+    );
+  }
+
+  Widget _fontLicense() {
+    return const Text("이 어플리케이션에는 교보 손글씨 2023 우선아 폰트를 사용되었습니다.");
   }
 }
